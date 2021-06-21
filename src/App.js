70336371e494a60f0e7ae6568bdef2a6084e8c52
tsahiBarshevsky <tsahi.13@gmail.com>
2021-06-21 11:37:53 +0300

    Products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => 
+{
+    const [products, setProducts] = useState([]);
+    // const [cart, setCart] = useState({});
+    // const [order, setOrder] = useState({});
+    // const [errorMessage, setErrorMessage] = useState('');
+
+    const fetchProducts = async () =>
+    {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        // fetchCart();
+    }, []);
+
+    return (
+        <div>
+            <Products products={products} />
+        </div>
+    )
 }
 
 export default App;
